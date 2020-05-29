14b0ad63329ed7db017af1a7ae154a5ea763176c
Matheus Martins <matheusmr00@gmail.com>
2020-05-29 15:43:44 -0300

    Updates

---
+++ versions/design-system/0.1.2/precache-manifest.11f94341e6ba4044bf73795f8695b9bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1821fec51f15986996359fdbef1e1b95",
+    "url": "./microfrontends/design-system/index.html"
+  },
+  {
+    "revision": "b67a0782a11de59774da",
+    "url": "./microfrontends/design-system/static/css/main.7abbd6b6.chunk.css"
+  },
+  {
+    "revision": "0d2ba7783f92ff804634",
+    "url": "./microfrontends/design-system/static/js/2.ea174cef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./microfrontends/design-system/static/js/2.ea174cef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b67a0782a11de59774da",
+    "url": "./microfrontends/design-system/static/js/main.38f2d3df.chunk.js"
+  },
+  {
+    "revision": "8dc3068fdb2853fb1503",
+    "url": "./microfrontends/design-system/static/js/runtime-main.7737ea44.js"
+  }
+]);
