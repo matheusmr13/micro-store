4a62476b634f85415676a685e8999e23b3fdacc3
Matheus Martins <matheusmr00@gmail.com>
2020-05-29 14:33:41 -0300

    Updates

---
+++ versions/cart/0.1.2/precache-manifest.d62c21cbdd4a69bbcc669f04b1a76910.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd60176c829dcb899c701f3498cbc929",
+    "url": "./microfrontends/cart/index.html"
+  },
+  {
+    "revision": "14b827401cacac8edc54",
+    "url": "./microfrontends/cart/static/css/main.9d7b5081.chunk.css"
+  },
+  {
+    "revision": "aceccb5e7371dd478b44",
+    "url": "./microfrontends/cart/static/js/2.d73ff75a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./microfrontends/cart/static/js/2.d73ff75a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14b827401cacac8edc54",
+    "url": "./microfrontends/cart/static/js/main.356a06da.chunk.js"
+  },
+  {
+    "revision": "8786e0a6fc9eca2f76d4",
+    "url": "./microfrontends/cart/static/js/runtime-main.b1bce558.js"
+  },
+  {
+    "revision": "55d4628348ab81ebb66d9d486caebcf3",
+    "url": "./microfrontends/cart/static/media/cart.55d46283.svg"
+  }
+]);
