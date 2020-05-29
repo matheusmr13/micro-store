919fa6377b59d94924fb00429ac22a1d5b9c2361
Matheus Martins <matheusmr00@gmail.com>
2020-05-29 15:45:38 -0300

    my commit

--- microfrontends/design-system/precache-manifest.11f94341e6ba4044bf73795f8695b9bf.js
+++ microfrontends/design-system/precache-manifest.11f94341e6ba4044bf73795f8695b9bf.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8de99afcacce6a78792be07e5763d178",
+    "revision": "1821fec51f15986996359fdbef1e1b95",
     "url": "./microfrontends/design-system/index.html"
   },
   {
-    "revision": "23453fb9fde4ce4c07c8",
-    "url": "./microfrontends/design-system/static/css/main.9da002b6.chunk.css"
+    "revision": "b67a0782a11de59774da",
+    "url": "./microfrontends/design-system/static/css/main.7abbd6b6.chunk.css"
   },
   {
     "revision": "0d2ba7783f92ff804634",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./microfrontends/design-system/static/js/2.ea174cef.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "23453fb9fde4ce4c07c8",
+    "revision": "b67a0782a11de59774da",
     "url": "./microfrontends/design-system/static/js/main.38f2d3df.chunk.js"
   },
   {
