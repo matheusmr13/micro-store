3c4d181505c01907746cc9ba1ff7ca4ed229283c
Matheus Martins <matheusmr00@gmail.com>
2020-05-29 14:07:11 -0300

    my commit

---
+++ microfrontends/cart/precache-manifest.4b9a76a7e9b0fc0f3f402b3e9261a9e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "531d5b2ccdb345bb350675051d82d02e",
+    "url": "./microfrontends/cart/index.html"
+  },
+  {
+    "revision": "6d2bc500a0fbb093017c",
+    "url": "./microfrontends/cart/static/css/main.9d7b5081.chunk.css"
+  },
+  {
+    "revision": "aceccb5e7371dd478b44",
+    "url": "./microfrontends/cart/static/js/2.d73ff75a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./microfrontends/cart/static/js/2.d73ff75a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d2bc500a0fbb093017c",
+    "url": "./microfrontends/cart/static/js/main.d4cd8be5.chunk.js"
+  },
+  {
+    "revision": "8786e0a6fc9eca2f76d4",
+    "url": "./microfrontends/cart/static/js/runtime-main.b1bce558.js"
+  },
+  {
+    "revision": "55d4628348ab81ebb66d9d486caebcf3",
+    "url": "./microfrontends/cart/static/media/cart.55d46283.svg"
+  }
+]);
